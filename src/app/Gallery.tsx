ac1dee0cd4b49575455a74fa33a5a110fd3c644b
rmisutyte <ruta.mi@protonmail.com>
2024-05-09 11:10:10 +0300

    placeholder images

---
+++ src/app/Gallery.tsx
@@ -0,0 +1,122 @@
+import styles from "./Gallery.module.css";
+
+export default function Gallery() {
+  return (
+    <div className={styles.gallery}>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+      <div className={styles.tile}>
+        <picture>
+          <source srcSet="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG" />
+          <img src="https://lonf-assets.s3.us-east-1.amazonaws.com/placeholder.JPG"></img>
+        </picture>
+      </div>
+    </div>
+  );
+}
