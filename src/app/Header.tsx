3230da75df1a665b2d1e1c20052596cd27b6574f
rmisutyte <ruta.mi@protonmail.com>
2024-05-10 17:50:33 +0300

    styling changes

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,7 +4,6 @@ export default function Header() {
   return (
     <header>
       <div className={styles.header}>
-        {/* className="h-screen flex items-center justify-center" */}
         <h1>Life on film</h1>
         <h2>Moments of life on film</h2>
       </div>
